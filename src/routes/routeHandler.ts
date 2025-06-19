4b869b54ea42625623bdf3ca16fb4c47fabd81ee
lifeoncode <fanele.zondi@outlook.com>
2025-06-19 14:54:04 +0200

    I think I'm done

--- src/routes/routeHandler.ts
+++ src/routes/routeHandler.ts
@@ -2,9 +2,23 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/status", (req, res) => {
+router.get("/status", async (req, res) => {
   try {
     res.send({ msg: "it works" });
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
+
+router.post("/word", async (req, res) => {
+  try {
+    const { data } = req.body;
+    if (!data.trim()) throw new Error("data required");
+
+    const wordArr: string[] = data.split("");
+    const sortedWordArr: string[] = wordArr.sort();
+    res.send({ word: sortedWordArr.join("") });
   } catch (error) {
     console.log(error);
     res.send(error);
